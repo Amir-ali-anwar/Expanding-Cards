fb73b221426aaae68adeed544e69d3cfb1e313d7
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-09-17 21:20:07 +0500

    feature completed

---
+++ app.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", (e) => {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+    panel.classList.add("active");
+  });
+});
+
+// function removeClass(){
+//     panels.forEach((panel)=>{
+//         panel.classList.remove('active')
+//     })
+// }
